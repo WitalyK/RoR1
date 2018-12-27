728aa686bfc24bdf6a8b02224a7a9d76085efbdb
WitalyK <o211ntrrrtt@gmail.com>
2018-12-27 10:33:23 +0300

    BD

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-require 'SQLite3'
+require 'sqlite3'
 
-db=SQLite3::Database.new 'mydatabase.sqlite'
+db=SQLite3::Database.new 'BarberShop.sqlite'
 
 db.execute "insert into Cars (Name, Price) values ('Запор', 50)"
 
